e9235e470a667a4acc5de17980ff0abfdb7e234e
Adam Moran <jeffadammoran@gmail.com>
2019-01-23 21:16:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,9 @@
 $ticket_number=0
 def take_a_number (deli)
   $ticket_number +=1
+  if $ticket_number = 100
+    $ticket_number = 0
+  end
   deli.push($ticket_number)
   number_in_line = deli[-1]
   puts "Welcome, You are number #{number_in_line} in line."
